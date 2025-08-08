cdf89a2024832f61900e0cdb1f9e472c24eca539
Reda AGAOUA <reda.agaoua@gmail.com>
2025-08-08 23:33:20 +0200

    refactor opts into a single parameter + only insert ar cursor position

--- lua/eco/init.lua
+++ lua/eco/init.lua
@@ -23,15 +23,16 @@ eco.setup = function(opts)
 	end
 end
 
---- Execute a shell command and paste its standard output at a given position.
+--- Execute a shell command and paste its standard output at the current cursor position.
 --- The command is executed asynchronously.
 ---
---- @param shell string shell interpreter for running the command
 --- @param cmd string The shell command to execute
---- @param row_idx integer 0-indexed line position at which the output will be inserted
---- @param col_idx integer 0-indexed column position at which the output will be inserted
+--- @param insert_after boolean If true, insert after cursor. if false, insert before cursor
+--- @param shell? string Shell interpreter for running the command. If not provided, use the user's default shell. If not set, use "sh"
 ---
-eco._insert_command_output = function(shell, cmd, row_idx, col_idx)
+eco._insert_command_output = function(cmd, insert_after, shell)
+	shell = shell or os.getenv("SHELL") or "sh"
+
 	vim.system({ shell, "-c", cmd }, {}, function(result)
 		if result.code ~= 0 then
 			error(string.format("Command '%s' failed with exit code %d :\n%s", cmd, result.code, result.stderr))
@@ -40,7 +41,7 @@ eco._insert_command_output = function(shell, cmd, row_idx, col_idx)
 
 		local lines = vim.split(result.stdout, "\n", { plain = true })
 		vim.schedule(function()
-			vim.api.nvim_buf_set_text(0, row_idx, col_idx, row_idx, col_idx, lines)
+			vim.api.nvim_put(lines, "c", insert_after, true)
 		end)
 	end)
 end
@@ -48,11 +49,8 @@ end
 --- Prompt the user for a shell command, execute it and insert the output after the current cursor position.
 --- The command is executed by the user's shell (identified by the `SHELL` env variable). If unset, use `sh`.
 ---
---- @class CmdOptions
---- @field insert_before? boolean If true, inserts before the current cursor position instead of the at current position
----
---- @param opts? CmdOptions Options
-eco._prompt_for_command = function(opts)
+--- @param insert_after boolean If true, insert after cursor. if false, insert before cursor
+eco._prompt_for_command = function(insert_after)
 	local shell = os.getenv("SHELL") or "sh"
 
 	vim.ui.input({
@@ -63,19 +61,7 @@ eco._prompt_for_command = function(opts)
 			return
 		end
 
-		local cursor_position = vim.api.nvim_win_get_cursor(0)
-
-		-- vim.api.nvim_win_get_cursor(...)[1] is the 1-indexed cursor row position
-		local row_idx = cursor_position[1] - 1
-		-- Insert after cursor by default (stays consistent with the way vim pastes text using `p` in normal mode).
-		local col_idx = cursor_position[2] + 1
-
-		opts = opts or {}
-		if opts.insert_before and col_idx > 0 then
-			col_idx = col_idx - 1
-		end
-
-		eco._insert_command_output(shell, input, row_idx, col_idx)
+		eco._insert_command_output(input, insert_after, shell)
 	end)
 end
 
--- plugin/eco.lua
+++ plugin/eco.lua
@@ -2,27 +2,27 @@
 ---
 --- This function is used internally by the `:Eco` and `:EcoBefore` user commands.
 --- If no arguments are provided, it prompts the user to input a shell command.
---- If arguments are provided, it executes the command directly.
+--- If arguments are provided, it treats those as a single command to be executed.
 ---
 --- @param createUserCmdOpts vim.api.keyset.create_user_command.command_args The options passed from `vim.api.nvim_create_user_command`,
---- @param ecoOpts? CmdOptions Options passed to the `eco` module (e.g., `{ insert_before = true }`)
-local prompt_or_execute_command = function(createUserCmdOpts, ecoOpts)
+--- @param insert_after boolean If true, insert after cursor. if false, insert before cursor
+local prompt_or_execute_command = function(createUserCmdOpts, insert_after)
 	local eco = require("eco")
 	if #createUserCmdOpts.fargs == 0 then
-		eco._prompt_for_command(ecoOpts)
+		eco._prompt_for_command(insert_after)
 	else
-		eco._insert_command_output(createUserCmdOpts.args, ecoOpts)
+		eco._insert_command_output(createUserCmdOpts.args, insert_after)
 	end
 end
 
 vim.api.nvim_create_user_command("Eco", function(opts)
-	prompt_or_execute_command(opts)
+	prompt_or_execute_command(opts, false)
 end, {
 	nargs = "*",
 })
 
 vim.api.nvim_create_user_command("EcoBefore", function(opts)
-	prompt_or_execute_command(opts, { insert_before = true })
+	prompt_or_execute_command(opts, true)
 end, {
 	nargs = "*",
 })
